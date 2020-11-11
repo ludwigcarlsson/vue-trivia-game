3893b52e313b8280012458cef51d2b72223376c7
Jesper Englund <jesper.englund@se.experis.com>
2020-11-11 13:45:37 +0100

    fixed questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,14 +1,74 @@
 <template>
+<div class = "questionContainer">
+  <div class = "questionBody">
+  <div class = "q1">
+      <h3>Question 1 of 10</h3>
+      <p>What country is the largest country in South America?</p>
+  </div>
+  <div class = "q2">
+      <h3>Question 2 of 10</h3>
+      <p>How long is an Olympic swimming pool?</p>
+  </div>
+  <div class = "q3">
+      <h3>Question 3 of 10</h3>
+      <p>In what year was Google launched on the web?</p>
+  </div>
+  <div class = "q4">
+      <h3>Question 4 of 10</h3>
+      <p>Who did the Mona Lisa paint?</p>
+  </div>
+  <div class = "q5">
+      <h3>Question 5 of 10</h3>
+      <p>What is the Turkish word for lamb?</p>
+  </div>
+  <div class = "q6">
+      <h3>Question 6 of 10</h3>
+      <p>Which bird can swim but cannot fly?</p>
+  </div>
+  <div class = "q7">
+      <h3>Question 7 of 10</h3>
+      <p>Who was the first president of the USA?</p>
+  </div>
+  <div class = "q8">
+      <h3>Question 8 of 10</h3>
+      <p>Which planet is nearest the sun?</p>
+  </div>
+  <div class = "q9">
+      <h3>Question 9 of 10</h3>
+      <p>What was Mozart s nationality?</p>
+  </div>
+  <div class = "q10">
+      <h3>Question 10 of 10</h3>
+      <p>Which country does the Peroni beer come from?</p>
+  </div>
+  </div>
+  <div class = "optionBody"></div>
+</div>
   
 </template>
 
 <script>
 
 export default {
-
+name: 'Question'
 }
 </script>
 
 <style>
+.questionBody {
+    background-color: cornflowerblue;
+    width: 300px;
+    border: 15px saddlebrown;
+    padding: 50px;
+    margin: 20px;
+}
+
+.optionBody {
+    background-color: orange;
+    width: 50px;
+
+
+
+}
 
 </style>
