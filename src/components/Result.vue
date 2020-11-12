769d9ec510d97001188bc423293da5b91439b3b0
Jesper Englund <jesper.englund@se.experis.com>
2020-11-12 15:26:08 +0100

    fixing result

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -3,11 +3,7 @@
         <fieldset class="result-section">
             <legend><h3>Game over</h3></legend>
             <h1>Your score:</h1>
-        </fieldset>
-        
-        <fieldset class="answer-section">
-             <legend><h3>Your scores question by question</h3></legend>
-            <p></p>
+            <h2>Your score question by question</h2>
         </fieldset>
 
         <fieldset class="highscore-section">
@@ -25,5 +21,30 @@ export default {
 </script>
 
 <style>
+body {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    background: #444;
+    margin: 0;
+}
+
+.result-section {
+    width: 60%;
+    height: 50%;
+    position: absolute;
+    left: 20%;
+    color: white;
+    margin-left: auto;
+}
+
+/*här behöver vi fixa så att det finns ett avstånd mellan result-section och highscore-section*/
+
+.highscore-section { 
+    width: 60%;
+    height: 30%; 
+    position: absolute;
+    left: 20%;
+    color: white;
+    margin-left: auto;
+}
 
 </style>
